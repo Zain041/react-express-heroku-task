e113bb6309636d95a15381a2975c2194f5596b05
Muhammad Zain <zaingulzar143gmail.com>
2020-12-22 21:12:56 +0500

    gdgdg

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ if(process.env.NODE_ENV === 'PRODUCTION'){
 	app.use(express.static('client/build'))
 }
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,' ./client','build')));
 
 app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname,'build','index.html'));
+	res.sendFile(path.join(__dirname,'./client','build','index.html'));
 })
 
 const port = process.env.PORT || 4000;
